1d46cc723e85a85c52fcdab2b3baeffdaaaf932e
GuilhermeFCO <guilherme.f.castro@ufv.br>
2023-11-17 20:08:25 -0300

    Add folder tailwind, bug the tailwind styles

--- packages/tailwind/tailwind.config.ts
+++ packages/tailwind/tailwind.config.ts
@@ -2,9 +2,8 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "tailwind-config/tailwind.config.ts";
 
-const config: Pick<Config, "prefix" | "presets"> = {
-  prefix: "ui-",
-  presets: [sharedConfig],
+const config: Pick<Config, "presets"> = {
+	presets: [sharedConfig],
 };
 
 export default config;
